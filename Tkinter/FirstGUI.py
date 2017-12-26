81475837735f978b9edcb6b9f4c27af71e706c2a
Engineer Amin Agrebi <amin.agrebi2012@gmail.com>
2017-12-26 16:29:23 +0100

    Update FirstGUI.py

--- Tkinter/FirstGUI.py
+++ Tkinter/FirstGUI.py
@@ -7,22 +7,12 @@ Recipe:  B04829_01_01
 # imports
 #======================
 import tkinter as tk
-from  tkinter import Menu
 
 # Create instance
 win = tk.Tk()   
 
 # Add a title       
 win.title("Python GUI")
-
-# Creating a Menu Bar
-menuBar = Menu(win)
-win.config(menu=menuBar)
-
-# Add menu items
-fileMenu = Menu(menuBar, tearoff=0)
-fileMenu.add_command(label="New")
-menuBar.add_cascade(label="File", menu=fileMenu)
 
 #======================
 # Start GUI
