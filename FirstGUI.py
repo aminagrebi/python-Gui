d51ce3578ba625e4070c020cf8f6bbe4dea9e7ec
Engineer Amin Agrebi <amin.agrebi2012@gmail.com>
2017-12-26 14:45:34 +0100

    Add files via upload

---
+++ FirstGUI.py
@@ -0,0 +1,30 @@
+'''
+May 25,  2017
+Recipe:  B04829_01_01
+@author: Amin Agrebi
+'''
+#======================
+# imports
+#======================
+import tkinter as tk
+from  tkinter import Menu
+
+# Create instance
+win = tk.Tk()   
+
+# Add a title       
+win.title("Python GUI")
+
+# Creating a Menu Bar
+menuBar = Menu(win)
+win.config(menu=menuBar)
+
+# Add menu items
+fileMenu = Menu(menuBar, tearoff=0)
+fileMenu.add_command(label="New")
+menuBar.add_cascade(label="File", menu=fileMenu)
+
+#======================
+# Start GUI
+#======================
+win.mainloop()
